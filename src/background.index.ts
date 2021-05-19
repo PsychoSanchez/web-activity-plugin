148529bf0dccaf2e2d3a956a5309ad91aab998c6
Aronov Aleksandr <aronov.aleks@yandex.ru>
2021-05-19 23:26:19 +0300

    #3 Moved activity tab tracker

--- src/background.index.ts
+++ src/background.index.ts
@@ -1,83 +1,10 @@
-import { addActivityTimeToHost } from './background/storage/accumulated-daily-activity';
-import {
-  ActiveTabState,
-  WindowActiveTabStateMonitor,
-} from './background/tracking/active-tab-monitor';
+import { WindowActiveTabStateMonitor } from './background/tracking/active-tab-monitor';
+import { ActiveTabTracker } from './background/tracking/active-tab.tracker';
 import {
   addGetActivityStoreMessageListener as addRuntimeStoreMessageListener,
   sendMessageWithStoreUpdate,
 } from './shared/background-browser-sync-storage';
-import {
-  BrowserSyncStorage,
-  createGlobalSyncStorageListener,
-} from './shared/browser-sync-storage';
-
-type FinishTrackingEvent = () => void;
-
-const startUrlTracker = (
-  url: string,
-  storage: BrowserSyncStorage
-): FinishTrackingEvent => {
-  const trackingStartDate = Date.now();
-  const { hostname } = new URL(url);
-
-  console.log(hostname, 'Active');
-
-  return () => {
-    const activityTime = Date.now() - trackingStartDate;
-
-    console.log(hostname, activityTime);
-
-    addActivityTimeToHost(storage, hostname, activityTime);
-  };
-};
-
-const startEmptyTracker = (): FinishTrackingEvent => {
-  console.log('Inactive');
-
-  return () => {};
-};
-
-const isInvalidUrl = (url: string | undefined): url is undefined => {
-  return !url || url.startsWith('chrome');
-};
-
-class ActiveTabTracker {
-  private invokeTrackingFinishEvent: FinishTrackingEvent = startEmptyTracker();
-
-  constructor(private storage: BrowserSyncStorage) {}
-
-  trackNewActiveTabState(tabState: ActiveTabState) {
-    this.invokeTrackingFinishEvent();
-
-    this.invokeTrackingFinishEvent =
-      this.createNewTrackerFromTabState(tabState);
-  }
-
-  private createNewTrackerFromTabState(
-    activeTabState: ActiveTabState
-  ): FinishTrackingEvent {
-    console.log('New State', activeTabState);
-
-    const activeTabUrl = activeTabState.lastActiveTab?.url;
-
-    if (
-      activeTabState.idleState === 'locked' ||
-      activeTabState.lastActiveTab === null ||
-      isInvalidUrl(activeTabUrl)
-    ) {
-      return startEmptyTracker();
-    }
-
-    if (activeTabState.idleState === 'idle') {
-      return activeTabState.lastActiveTab.audible
-        ? startUrlTracker(activeTabUrl, this.storage)
-        : startEmptyTracker();
-    }
-
-    return startUrlTracker(activeTabUrl, this.storage);
-  }
-}
+import { createGlobalSyncStorageListener } from './shared/browser-sync-storage';
 
 try {
   const storage = createGlobalSyncStorageListener();
---
+++ src/background/tracking/active-tab.tracker.ts
@@ -0,0 +1,70 @@
+import { BrowserSyncStorage } from '../../shared/browser-sync-storage';
+import { addActivityTimeToHost } from '../storage/accumulated-daily-activity';
+import { ActiveTabState } from './active-tab-monitor';
+
+type FinishTrackingEvent = () => void;
+
+const startUrlTracker = (
+  url: string,
+  storage: BrowserSyncStorage
+): FinishTrackingEvent => {
+  const trackingStartDate = Date.now();
+  const { hostname } = new URL(url);
+
+  console.log(hostname, 'Active');
+
+  return () => {
+    const activityTime = Date.now() - trackingStartDate;
+
+    console.log(hostname, activityTime);
+
+    addActivityTimeToHost(storage, hostname, activityTime);
+  };
+};
+
+const startEmptyTracker = (): FinishTrackingEvent => {
+  console.log('Inactive');
+
+  return () => {};
+};
+
+const isInvalidUrl = (url: string | undefined): url is undefined => {
+  return !url || url.startsWith('chrome');
+};
+
+export class ActiveTabTracker {
+  private invokeTrackingFinishEvent: FinishTrackingEvent = startEmptyTracker();
+
+  constructor(private storage: BrowserSyncStorage) {}
+
+  trackNewActiveTabState(tabState: ActiveTabState) {
+    this.invokeTrackingFinishEvent();
+
+    this.invokeTrackingFinishEvent =
+      this.createNewTrackerFromTabState(tabState);
+  }
+
+  private createNewTrackerFromTabState(
+    activeTabState: ActiveTabState
+  ): FinishTrackingEvent {
+    console.log('New State', activeTabState);
+
+    const activeTabUrl = activeTabState.lastActiveTab?.url;
+
+    if (
+      activeTabState.idleState === 'locked' ||
+      activeTabState.lastActiveTab === null ||
+      isInvalidUrl(activeTabUrl)
+    ) {
+      return startEmptyTracker();
+    }
+
+    if (activeTabState.idleState === 'idle') {
+      return activeTabState.lastActiveTab.audible
+        ? startUrlTracker(activeTabUrl, this.storage)
+        : startEmptyTracker();
+    }
+
+    return startUrlTracker(activeTabUrl, this.storage);
+  }
+}
