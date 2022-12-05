f16590756f989c32c03ee6e20d81a2de15435054
Aronov Aleksandr <aronov.aleks@yandex.ru>
2022-12-05 21:57:11 +0200

    Fixed undefined fs in the build script

--- tools/release.js
+++ tools/release.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 // Update manifest.json to package.json version
 const manifest = require(__dirname + '/../static/manifest.json');
 const package = require(__dirname + '/../package.json');
