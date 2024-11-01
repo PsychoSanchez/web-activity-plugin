c94d5ee97b978cb0d9dbe0c7862d983863f46bd6
Aronov Aleksandr <aronov.aleks@yandex.ru>
2024-11-01 02:10:43 +0100

    Updated spec tests

--- src/shared/utils/url.ts
+++ src/shared/utils/url.ts
@@ -1,3 +1,5 @@
+import { assert } from './guards';
+
 export const getHostNameFromUrl = (url: string) => {
   const { hostname } = new URL(url);
 
@@ -16,3 +18,16 @@ const BROWSER_URL_PREFIX = [
 export const isInvalidUrl = (url: string | undefined): url is undefined => {
   return !url || BROWSER_URL_PREFIX.some((broName) => url.startsWith(broName));
 };
+
+// Validate that domain is valid (example.com)
+const DOMAIN_REGEX = /^([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}$/;
+
+export type Domain = string & { readonly __domain: unique symbol };
+
+export function assertIsValidHostname(
+  domain?: string,
+): asserts domain is Domain {
+  // Validate domain
+  assert(domain, 'Domain is required');
+  assert(DOMAIN_REGEX.test(domain), 'Domain is invalid');
+}
