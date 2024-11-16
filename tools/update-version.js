f680b4cd718de4a7d3ed20ca9f72157a8423668f
Alexander Aronov <aronov.aleks@outlook.com>
2024-11-16 18:23:30 +0100

    Added Release Workflow (#40)
    * Added release workflow

--- tools/update-version.js
+++ tools/update-version.js
@@ -1,4 +1,11 @@
 import fs from 'fs';
+import { createRequire } from 'module';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const require = createRequire(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Update manifest.json to package.json version
 const manifest = require(__dirname + '/../static/manifest.json');
